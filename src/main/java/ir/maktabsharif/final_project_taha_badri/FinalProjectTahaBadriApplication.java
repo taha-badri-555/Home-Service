44adfc3f2ed549783e231c8dc71d0eabf84bf421
taha-badri-555 <tahabadri82@gmail.com>
2025-07-01 19:20:01 +0330

    Add main class.

--- src/main/java/ir/maktabsharif/final_project_taha_badri/FinalProjectTahaBadriApplication.java
+++ src/main/java/ir/maktabsharif/final_project_taha_badri/FinalProjectTahaBadriApplication.java
@@ -1,7 +1,11 @@
 package ir.maktabsharif.final_project_taha_badri;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FinalProjectTahaBadriApplication {
@@ -10,4 +14,10 @@ public class FinalProjectTahaBadriApplication {
         SpringApplication.run(FinalProjectTahaBadriApplication.class, args);
     }
 
+    @Bean
+    public EntityManager entityManager() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("FinalProjectTahaBadri");
+        return emf.createEntityManager();
+
+    }
 }
