abd25716a529d18ee47cfb04f046887766cb405a
taha-badri-555 <tahabadri82@gmail.com>
2025-07-01 19:18:09 +0330

    Add enums

---
+++ src/main/java/ir/maktabsharif/final_project_taha_badri/domain/enums/OrderStatus.java
@@ -0,0 +1,11 @@
+package ir.maktabsharif.final_project_taha_badri.domain.enums;
+
+public enum OrderStatus {
+
+    WAITING_FOR_EXPERT_PROPOSAL,
+    WAITING_FOR_EXPERT_TO_VISIT,
+    STARTED,
+    DONE,
+    PAYED
+
+}
