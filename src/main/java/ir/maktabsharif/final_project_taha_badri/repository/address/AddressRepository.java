93d18f97bbc1459869af80d5f1e09295858de432
taha-badri-555 <tahabadri82@gmail.com>
2025-07-15 14:45:10 +0330

    refactor AddressRepository

--- src/main/java/ir/maktabsharif/final_project_taha_badri/repository/address/AddressRepository.java
+++ src/main/java/ir/maktabsharif/final_project_taha_badri/repository/address/AddressRepository.java
@@ -1,9 +1,19 @@
 package ir.maktabsharif.final_project_taha_badri.repository.address;
 
 import ir.maktabsharif.final_project_taha_badri.domain.entity.Address;
-import ir.maktabsharif.final_project_taha_badri.repository.base.crud.CrudRepository;
+import ir.maktabsharif.final_project_taha_badri.domain.entity.user.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AddressRepository extends CrudRepository<Address, Long> {
+public interface AddressRepository
+        extends JpaRepository<Address, Long>, JpaSpecificationExecutor<Address> {
+@Modifying
+@Query("""
+        update Address a  set a.customer=:customer where a.id=:addressId
+        """)
+    void addAddressToCustomerAddressesByCustomerIdAndAddress(Customer customer, Long addressId);
 }
