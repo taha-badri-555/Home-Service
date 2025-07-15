c72e5d8ecb7827c03237bd76ea99dbbf368f4c8a
taha-badri-555 <tahabadri82@gmail.com>
2025-07-15 14:44:26 +0330

    refactor OrderStatus

--- src/main/java/ir/maktabsharif/final_project_taha_badri/domain/enums/OrderStatus.java
+++ src/main/java/ir/maktabsharif/final_project_taha_badri/domain/enums/OrderStatus.java
@@ -1,7 +1,7 @@
 package ir.maktabsharif.final_project_taha_badri.domain.enums;
 
 public enum OrderStatus {
-
+    WAITING_TO_CHOOSE_EXPERT,
     WAITING_FOR_EXPERT_PROPOSAL,
     WAITING_FOR_EXPERT_TO_VISIT,
     STARTED,
