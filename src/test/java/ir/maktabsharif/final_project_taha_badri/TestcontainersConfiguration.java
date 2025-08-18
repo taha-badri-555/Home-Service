d4b4944dc21ddd272de68bc48585579942be5c89
taha <Tahabadri83@gmail.com>
2025-08-18 11:52:08 +0330

    delete: old DTO and test

--- src/test/java/ir/maktabsharif/final_project_taha_badri/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package ir.maktabsharif.final_project_taha_badri;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
